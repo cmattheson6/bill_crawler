a72c8332191b81fb638fb6530548a36bfec0606d
cmattheson6 <42688579+cmattheson6@users.noreply.github.com>
2018-09-25 09:18:14 -0400

    Update pipelines.py

--- congress_bill_info/pipelines.py
+++ congress_bill_info/pipelines.py
@@ -24,11 +24,48 @@ class CongressBillInfoPipeline(object):
             return item
         # Uploads bill items
         else:
-            # Attempt to upload item to database
-            try:
                 
             #ADD IN GCP SCRIPT HERE
-                pass
+                cred_dict = {
+                                 "auth_provider_x509_cert_url": spider.settings.get('auth_provider_x509_cert_url'),
+                                 "auth_uri": spider.settings.get('auth_uri'),
+                                 "client_email": spider.settings.get('client_email'),
+                                 "client_id": spider.settings.get('client_id'),
+                                 "client_x509_cert_url": spider.settings.get('client_x509_cert_url'),
+                                 "private_key": spider.settings.get('private_key'),
+                                 "private_key_id": spider.settings.get('private_key_id'),
+                                 "project_id": spider.settings.get('project_id'),
+                                 "token_uri": spider.settings.get('token_uri'),
+                                 "type": spider.settings.get('account_type')
+                     }
+                cred_dict['private_key'] = cred_dict['private_key'].replace('\\n', '\n')
+                print(cred_dict)
+
+                credentials = service_account.Credentials.from_service_account_info(cred_dict)
+                print(credentials)
+                print("I haven't set up the client yet, but I built the credentials!")
+                publisher = pubsub.PublisherClient(credentials = credentials)
+                print(publisher)
+                print("The client was set up!")
+
+                topic = 'projects/{project_id}/topics/{topic}'.format(
+                     project_id='politics-data-tracker-1',
+                     topic='house_pols')
+                project_id = 'politics-data-tracker-1'
+                topic_name = 'bill_info'
+                topic_path = publisher.topic_path(project_id, topic_name)
+                data = u'This is a representative in the House.'
+                data = data.encode('utf-8')
+                print("The topic was built!")
+                publisher.publish(topic_path, data=data,
+                                  bill_id = item['bill_id'],
+                                  amdt_id = item['amdt_id'],
+                                  bill_title = item['bill_title'],
+                                  bill_summary = item['bill_summary'],
+                                  sponsor_id = item['sponsor_id'],
+                                  bill_url = item['bill_url'])
+                print("We published! WOOOO!")
+                
                 return item
 
 # This pipeline is only to upload the bill cosponsors
@@ -43,7 +80,41 @@ class BillCosponsorsPipeline(object):
         if None in cosponsor_packet:
             pass
         else:
-            # Attempt to upload packet to database
-            try:
-                pass
+                cred_dict = {
+                                 "auth_provider_x509_cert_url": spider.settings.get('auth_provider_x509_cert_url'),
+                                 "auth_uri": spider.settings.get('auth_uri'),
+                                 "client_email": spider.settings.get('client_email'),
+                                 "client_id": spider.settings.get('client_id'),
+                                 "client_x509_cert_url": spider.settings.get('client_x509_cert_url'),
+                                 "private_key": spider.settings.get('private_key'),
+                                 "private_key_id": spider.settings.get('private_key_id'),
+                                 "project_id": spider.settings.get('project_id'),
+                                 "token_uri": spider.settings.get('token_uri'),
+                                 "type": spider.settings.get('account_type')
+                     }
+                cred_dict['private_key'] = cred_dict['private_key'].replace('\\n', '\n')
+                print(cred_dict)
+
+                credentials = service_account.Credentials.from_service_account_info(cred_dict)
+                print(credentials)
+                print("I haven't set up the client yet, but I built the credentials!")
+                publisher = pubsub.PublisherClient(credentials = credentials)
+                print(publisher)
+                print("The client was set up!")
+
+                topic = 'projects/{project_id}/topics/{topic}'.format(
+                     project_id='politics-data-tracker-1',
+                     topic='cosponsors')
+                project_id = 'politics-data-tracker-1'
+                topic_name = 'house_pols'
+                topic_path = publisher.topic_path(project_id, topic_name)
+                data = u'This is a representative in the House.'
+                data = data.encode('utf-8')
+                print("The topic was built!")
+                publisher.publish(topic_path, data=data,
+                                  bill_id = item['bill_id'],
+                                  amdt_id = item['amdt_id'],
+                                  cosponsor_id = item['cosponsor_id'],
+                                  cosponsor_info = item['cosponsor_info'])
+                print("We published! WOOOO!")
                 return item;
