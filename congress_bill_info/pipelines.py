70a49f282e69e84742edf0066f1cb59965e4b31d
cmattheson6 <cmattheson6@gmail.com>
2019-03-07 08:52:38 -0500

    Refreshed and refactored scripts based on Dataflow scripts.

--- congress_bill_info/pipelines.py
+++ congress_bill_info/pipelines.py
@@ -1,19 +1,20 @@
-# -*- coding: utf-8 -*-
+"""
+This pipeline will upload all items from the spiders to the proper Pub/Sub topic.
+The rest of the processing will take place in Dataflow.
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+In order to handle two different types of dictionaries that need to go to two different topics, they need to be
+handled accordingly:
+ - There are two separate pipelines in the settings.py file: CongressBillInfoPipeline and BillCosponsorsPipeline
+ - Check that the keys in the item exactly match the keys we are looking for.
+ - If they check out, then it's good to go. If not, it gets passed to the other pipeline.
 
+2)
+"""
 ### -------- Import all necessary modules -------- ###
 import scrapy
 from google.cloud import pubsub
 from google.oauth2 import service_account
-import subprocess
-import scrapy
-import scrapy.crawler
-from scrapy.utils.project import get_project_settings
-import google.auth
+import logging
 
 ### -------- Start of the pipeline -------- ###
 
@@ -21,117 +22,141 @@ import google.auth
 class CongressBillInfoPipeline(object):
     # Uploads bill information to the database
     def process_item(self, item, spider):
-        bill_packet = (item.setdefault('bill_id', None), 
-                       item.setdefault('bill_title', None), 
-                       item.setdefault('sponsor_fn', None), 
-                       item.setdefault('sponsor_ln', None), 
-                       item.setdefault('sponsor_party', None), 
-                       item.setdefault('sponsor_state', None), 
-                       item.setdefault('bill_url', None))
-        # Filters out any cosponsor items and only uploads bill items
-        if None in list(bill_packet):
-            return item
-        # Uploads bill items
+        # item = (
+        #     item.setdefault('bill_id', None),
+        #     item.setdefault('bill_title', None),
+        #     item.setdefault('sponsor_fn', None),
+        #     item.setdefault('sponsor_ln', None),
+        #     item.setdefault('sponsor_party', None),
+        #     item.setdefault('sponsor_state', None),
+        #     item.setdefault('bill_url', None))
+        bill_info_keys = ['bill_id',
+                            'amdt_id'
+                            'bill_title',
+                            'bill_summary',
+                            'sponsor_fn',
+                            'sponsor_ln',
+                            'sponsor_party',
+                            'sponsor_state',
+                            'bill_url']
+        # Filters out any cosponsor items and only uploads bill items.
+        if all([i in bill_info_keys for i in item.keys()]):
+            """We need to establish a an authorized connection to Google Cloud in order to upload to Google Pub/Sub.
+            In order to host the spiders on Github, the service account credentials are housed on the Scrapy platform
+            and dynamically created in the script."""
+            # Pull all of the credential info from the Scrapy platform into a dictionary.
+            cred_dict = {
+                         "auth_provider_x509_cert_url": spider.settings.get('auth_provider_x509_cert_url'),
+                         "auth_uri": spider.settings.get('auth_uri'),
+                         "client_email": spider.settings.get('client_email'),
+                         "client_id": spider.settings.get('client_id'),
+                         "client_x509_cert_url": spider.settings.get('client_x509_cert_url'),
+                         "private_key": spider.settings.get('private_key'),
+                         "private_key_id": spider.settings.get('private_key_id'),
+                         "project_id": spider.settings.get('project_id'),
+                         "token_uri": spider.settings.get('token_uri'),
+                         "type": spider.settings.get('account_type')
+                 }
+            logging.info('Credentials downloaded from Scrapy server.')
+            cred_dict['private_key'] = cred_dict['private_key'].replace('\\n', '\n')
+
+            # Build a Credentials object from the above dictionary. This will properly allow access as part of a
+            # Google Cloud Client.
+            credentials = service_account.Credentials.from_service_account_info(cred_dict)
+            logging.info('Credentials object created.')
+
+            # Create Publisher client.
+            publisher = pubsub.PublisherClient(credentials=credentials)
+            logging.info('Publisher Client created.')
+
+            # Set location of proper publisher topic
+            project_id = 'politics-data-tracker-1'
+            topic_name = 'bill_info'
+            topic_path = publisher.topic_path(project_id, topic_name)
+            data = u'This is a representative in the House.' #Consider how to better use this.
+            data = data.encode('utf-8')
+            publisher.publish(
+                topic_path,
+                data=data,
+                bill_id = item['bill_id'],
+                amdt_id = item['amdt_id'],
+                bill_title = item['bill_title'],
+                bill_summary = item['bill_summary'],
+                sponsor_fn = item['sponsor_fn'],
+                sponsor_ln = item['sponsor_ln'],
+                sponsor_state = item['sponsor_state'],
+                sponsor_party = item['sponsor_party'],
+                bill_url = item['bill_url'])
+            logging.info('Published item: {0}'.format(item))
+            yield item
+        # If not true, sends the item to the next pipeline as-is.
         else:
-                
-            #ADD IN GCP SCRIPT HERE
-                cred_dict = {
-                                 "auth_provider_x509_cert_url": spider.settings.get('auth_provider_x509_cert_url'),
-                                 "auth_uri": spider.settings.get('auth_uri'),
-                                 "client_email": spider.settings.get('client_email'),
-                                 "client_id": spider.settings.get('client_id'),
-                                 "client_x509_cert_url": spider.settings.get('client_x509_cert_url'),
-                                 "private_key": spider.settings.get('private_key'),
-                                 "private_key_id": spider.settings.get('private_key_id'),
-                                 "project_id": spider.settings.get('project_id'),
-                                 "token_uri": spider.settings.get('token_uri'),
-                                 "type": spider.settings.get('account_type')
-                     }
-                cred_dict['private_key'] = cred_dict['private_key'].replace('\\n', '\n')
-                print(cred_dict)
-
-                credentials = service_account.Credentials.from_service_account_info(cred_dict)
-                print(credentials)
-                print("I haven't set up the client yet, but I built the credentials!")
-                publisher = pubsub.PublisherClient(credentials = credentials)
-                print(publisher)
-                print("The client was set up!")
-
-                topic = 'projects/{project_id}/topics/{topic}'.format(
-                     project_id='politics-data-tracker-1',
-                     topic='house_pols')
-                project_id = 'politics-data-tracker-1'
-                topic_name = 'bill_info'
-                topic_path = publisher.topic_path(project_id, topic_name)
-                data = u'This is a representative in the House.'
-                data = data.encode('utf-8')
-                print("The topic was built!")
-                publisher.publish(topic_path, data=data,
-                                  bill_id = str(item['bill_id']),
-                                  amdt_id = str(item['amdt_id']),
-                                  bill_title = str(item['bill_title']),
-                                  bill_summary = str(item['bill_summary']),
-                                  sponsor_fn = str(item['sponsor_fn']),
-                                  sponsor_ln = str(item['sponsor_ln']),
-                                  sponsor_state = str(item['sponsor_state']),
-                                  sponsor_party = str(item['sponsor_party']),
-                                  bill_url = str(item['bill_url']))
-                print("We published! WOOOO!")
-                
-                return item
+            yield item
 
 # This pipeline is only to upload the bill cosponsors
 class BillCosponsorsPipeline(object):
     
     # Builds and uploads query
     def process_item(self, item, spider):
-        cosponsor_packet = (item.setdefault('bill_id', None),
-                            item.setdefault('cosponsor_fn', None),
-                            item.setdefault('cosponsor_ln', None),
-                            item.setdefault('cosponsor_party', None),
-                            item.setdefault('cosponsor_state', None))
+        # cosponsor_packet = (item.setdefault('bill_id', None),
+        #                     item.setdefault('cosponsor_fn', None),
+        #                     item.setdefault('cosponsor_ln', None),
+        #                     item.setdefault('cosponsor_party', None),
+        #                     item.setdefault('cosponsor_state', None))
         
         # Filters out all bill items
-        if None in list(cosponsor_packet):
-            return item
+        cosponsor_keys = ['bill_id',
+                          'amdt_id',
+                          'cosponsor_fn',
+                          'cosponsor_ln',
+                          'cosponsor_party',
+                          'cosponsor_state']
+        # Filters out any cosponsor items and only uploads bill items.
+        if all([i in cosponsor_keys for i in item.keys()]):
+            # Pull all of the credential info from the Scrapy platform into a dictionary.
+            cred_dict = {
+                         "auth_provider_x509_cert_url": spider.settings.get('auth_provider_x509_cert_url'),
+                         "auth_uri": spider.settings.get('auth_uri'),
+                         "client_email": spider.settings.get('client_email'),
+                         "client_id": spider.settings.get('client_id'),
+                         "client_x509_cert_url": spider.settings.get('client_x509_cert_url'),
+                         "private_key": spider.settings.get('private_key'),
+                         "private_key_id": spider.settings.get('private_key_id'),
+                         "project_id": spider.settings.get('project_id'),
+                         "token_uri": spider.settings.get('token_uri'),
+                         "type": spider.settings.get('account_type')
+                 }
+            logging.info('Credentials downloaded from Scrapy server.')
+            cred_dict['private_key'] = cred_dict['private_key'].replace('\\n', '\n')
+
+            # Build a Credentials object from the above dictionary. This will properly allow access as part of a
+            # Google Cloud Client.
+            credentials = service_account.Credentials.from_service_account_info(cred_dict)
+            logging.info('Credentials object created.')
+
+            # Create Publisher client.
+            publisher = pubsub.PublisherClient(credentials=credentials)
+            logging.info('Publisher Client created.')
+
+            # Set location of proper publisher topic
+            project_id = 'politics-data-tracker-1'
+            topic_name = 'cosponsors'
+            topic_path = publisher.topic_path(project_id, topic_name)
+
+            #Figure out how to use this better.
+            data = u'This is the cosponsors related to bill {0}.'.format(item['bill_id'])
+            data = data.encode('utf-8')
+            publisher.publish(
+                topic_path,
+                data=data,
+                bill_id = item['bill_id'],
+                amdt_id = item['amdt_id'],
+                sponsor_fn = item['cosponsor_fn'],
+                sponsor_ln = item['cosponsor_ln'],
+                sponsor_state = item['cosponsor_state'],
+                sponsor_party = item['cosponsor_party'])
+            logging.info('Published item: {0}'.format(item))
+            yield item
+        # If not true, sends the item to the next pipeline as-is.
         else:
-                cred_dict = {
-                                 "auth_provider_x509_cert_url": spider.settings.get('auth_provider_x509_cert_url'),
-                                 "auth_uri": spider.settings.get('auth_uri'),
-                                 "client_email": spider.settings.get('client_email'),
-                                 "client_id": spider.settings.get('client_id'),
-                                 "client_x509_cert_url": spider.settings.get('client_x509_cert_url'),
-                                 "private_key": spider.settings.get('private_key'),
-                                 "private_key_id": spider.settings.get('private_key_id'),
-                                 "project_id": spider.settings.get('project_id'),
-                                 "token_uri": spider.settings.get('token_uri'),
-                                 "type": spider.settings.get('account_type')
-                     }
-                cred_dict['private_key'] = cred_dict['private_key'].replace('\\n', '\n')
-                print(cred_dict)
-
-                credentials = service_account.Credentials.from_service_account_info(cred_dict)
-                print(credentials)
-                print("I haven't set up the client yet, but I built the credentials!")
-                publisher = pubsub.PublisherClient(credentials = credentials)
-                print(publisher)
-                print("The client was set up!")
-
-                topic = 'projects/{project_id}/topics/{topic}'.format(
-                     project_id='politics-data-tracker-1',
-                     topic='cosponsors')
-                project_id = 'politics-data-tracker-1'
-                topic_name = 'cosponsors'
-                topic_path = publisher.topic_path(project_id, topic_name)
-                data = u'This is the cosponsors related to bill {0}.'.format(item['bill_id'])
-                data = data.encode('utf-8')
-                print("The topic was built!")
-                publisher.publish(topic_path, data=data,
-                                  bill_id = str(item['bill_id']),
-                                  amdt_id = str(item['amdt_id']),                                  
-                                  sponsor_fn = str(item['cosponsor_fn']),
-                                  sponsor_ln = str(item['cosponsor_ln']),
-                                  sponsor_state = str(item['cosponsor_state']),
-                                  sponsor_party = str(item['cosponsor_party']))
-                print("We published! WOOOO!")
-                return item;
+            yield item
--- congress_bill_info/spiders/bill_crawler.py
+++ congress_bill_info/spiders/bill_crawler.py
@@ -1,16 +1,19 @@
-# -*- coding: utf-8 -*-
+'''
+This Scrapy pipeline will have a daily pull of the current day's bills that are presented to Congress. This data comes
+directly from the Congressional website. This will be automated for daily pulls in order to keep the
+current bill list up to date.
+'''
 
 ### -------- Import all of the necessary files -------- ###
-import scrapy # for scraping
-# For matching characters
+import scrapy
 import re
-import unidecode # MAKE SURE THAT ALL FILES ARE ACTUALLY HERE *** NO. 3 THING TO DO TOMORROW
+from datetime import datetime, timedelta
+from datetime import date
 
 ### -------- Define all custom fxns here -------- ###
 
-# Pull from a raw string the relevant info on a politician (first name, last name, party, state)
-
-# Find the location of needed character in a string
+# Find the location of needed character in a string.
+# Nothing in the 're' package does exactly what was needed for this fxn.
 def find_character(s, ch):
     index_nums = []
     index = 0
@@ -19,8 +22,8 @@ def find_character(s, ch):
             index_nums.append(index)
             index = index + 1
         else:       
-            index = index + 1;
-    return index_nums;
+            index = index + 1
+    return index_nums
 
 def create_pol_dict(pol):
     regex = '[^a-zA-Z]'
@@ -32,12 +35,13 @@ def create_pol_dict(pol):
         pol_state = s[find_character(s, '-')[0]+1:find_character(s, '-')[1]]
     elif s.count('-') == 1:
         pol_state = s[find_character(s, '-')+1:len(s)]
-    else: Error
+    else:
+        raise IndexError('String formatting does not fit pre-defined formats.')
     pol_dict = {'first_name': pol_fn,
                 'last_name': pol_ln,
                 'party': pol_party,
                 'state': pol_state}
-    return pol_dict;
+    return pol_dict
 def create_bill_dict(b): #inputs raw bill info
     if b.count('-') > 0:
         bill_id = b[0:b.index("-")].replace(" ", "").replace(".", "")
@@ -54,7 +58,16 @@ def create_bill_dict(b): #inputs raw bill info
         bill_dict = {'bill_id': bill_id,
                      'amdt_id': amdt_id,
                      'bill_title': bill_title}
-        return bill_dict;
+        return bill_dict
+
+
+# Set date for yesterday's bills that are published
+date_yesterday = date.today() - timedelta(days=1)
+this_year = date_yesterday.year
+
+# Automates the changing of the URLs every year.
+congress_num = ((this_year - 1789)/2) + 1
+session_num = 2 if this_year % 2 == 0 else 1
 
 ### -------- Start of the spider -------- ###
 
@@ -62,58 +75,64 @@ class BillCrawlerSpider(scrapy.Spider):
     # Set the name of the spider and the corresponding starting points of the scrape
     name = 'bill_crawler'
     allowed_domains = ['www.congress.gov']
-    start_urls = ['https://www.congress.gov/search?q=%7B%22congress%22%3A%22115%22%2C%22source%22%3A%22legislation%22%7D&searchResultViewType=expanded&page=1']
+    start_urls = ['''https://www.congress.gov/search?q=%7B%22congress%22%3A%22{0}%22%
+                  2C%22source%22%3A%22legislation%22%7D&searchResultViewType=expanded&page=1'''
+                      .format(congress_num)]
 
-#extrapolate the next level up: the list of all bills in this cycle
-#add in the correct URL
-#parse out the links to the bills
-#have that feed into the below parse function
-    # The first parse will pull all URLs from the page that link to individual bills
+    # The first parse will pull all URLs from the page that link to individual bills.
     def parse(self, response):
-        list_of_urls = response.xpath('.//a/@href').re(r'^.*/bill/.*/.*/[0-9]*\?.*$')
-        bill_urls = list_of_urls[0::2]
-        print(bill_urls) ###formula checkpoint
-        for u in bill_urls:
-            url = u[0:re.search(r'\?', u).start()]
-            yield scrapy.Request(url = url,
-                         callback = self.parse_bill);
-            # The loop should stop here based on if the date provided was not from yesterday
-#             bill_date = ''
-#             if bill_date > datetime.timedelta(days = 1): ### REVISE HERE
-#                 print('URL in list. All new bills have been uploaded.')
-#                 break
-#             else:
-#                 yield scrapy.Request(url = url,
-#                                      callback = self.parse_bill);
+        bill_table = response.xpath('.//li[@class="expanded"]')
+        for i in bill_table:
+            # Pulls and formats the bill date.
+            bill_date = i.xpath('.//span[@class="result-item"/text()').extract()[2]
+            bill_date = datetime.strptime(bill_date, ' (Introduced %m/%d/%Y) ')
+            bill_date = bill_date.date()
+
+            # Pulls and formats the bill's URL
+            bill_url = i.xpath('.//a/@href').re_first(r'^.*/bill/.*/.*/[0-9]*\?.*$')
+            bill_url = bill_url[0:re.search(r'\?', bill_url).start()]
+
+            # The loop should stop here based on if the date of a bill was not from yesterday.
+            if bill_date > date_yesterday:
+                pass
+            elif bill_date == date_yesterday:
+                yield scrapy.Request(url = bill_url,
+                                     callback = self.parse_bill)
+            else:
+                yield scrapy.Request(url = bill_url,
+                                     callback = self.parse_bill) # Change to break after first pass at scraping site.
         
-#         # Because of the set-up of the site, this section should only be used once to build a full database the first time                                
-#         # Determine if there is a next page link, what that link is
-#         next_page = response.xpath(".//a[@class='next']/@href").extract_first()
+        # Because of the set-up of the site,
+        # this section should only be used once to build a full database the first time
+        # Determine if there is a next page link, what that link is
+        next_page = response.xpath(".//a[@class='next']/@href").extract_first()
         
-#         # Follow the next_page link from the top of the spider if available
-#         if next_page is not None:
-#             next_page = response.urljoin(next_page)
-#             yield scrapy.Request(url = next_page,
-#                                  callback = self.parse)
-                                        
+        # Follow the next_page link from the top of the spider if available
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(url = next_page,
+                                 callback = self.parse)
 
     # The second parse will pull out all of the bill's relevant information
     def parse_bill(self, response):
-        # Parses out all of the bill info from the page
+        # Parses out all of the bill info from the page.
         bill_info = response.xpath(".//h1[@class='legDetail']/text()").extract_first()
         bill_info = create_bill_dict(bill_info)
-        sponsor_info = response.xpath(".//table[@class='standard01']/tr/td/a/text()").re_first(r'^.*\[.*\]$')
-        bill_summary = response.xpath(".//div[@id='bill-summary']/p/text()").extract_first()
-        
+
         bill_id = bill_info['bill_id']
         amdt_id = bill_info['amdt_id']
         bill_title = bill_info['bill_title']
-        
-        bill_sec_title = response.xpath(".//table[@class='standard01']/tr/td/text()").extract_first()
-        bill_title = bill_title + bill_sec_title
+
+        bill_subtitle = response.xpath(".//table[@class='standard01']/tr/td/text()").extract_first()
+        bill_title = bill_title + bill_subtitle
 
         # Get all sponsor info parsed from raw info
+        sponsor_info = response.xpath(".//table[@class='standard01']/tr/td/a/text()").re_first(r'^.*\[.*\]$')
         sponsor_info = create_pol_dict(sponsor_info)
+
+        # Parse the summary description of the bill.
+        bill_summary = response.xpath(".//div[@id='bill-summary']/p/text()").extract_first()
+
 
         # Assign all parts of the bill to a dictionary for pipeline discovery        
         bill_dict = {'bill_id': bill_id, 
@@ -127,16 +146,17 @@ class BillCrawlerSpider(scrapy.Spider):
                      'bill_url': response.request.url}
                 
         #Pull the link to the cosponsors of the bill.
-        partial_link = response.xpath(".//a[contains(text(), 'Cosponsors')]/@href").extract_first()
-        cosponsors_link = response.urljoin(partial_link)
-        cosponsors_request = scrapy.Request(url = cosponsors_link, 
+        url = response.xpath(".//a[contains(text(), 'Cosponsors')]/@href").extract_first()
+        url = response.urljoin(url)
+        cosponsors_request = scrapy.Request(url = url,
                                             callback = self.parse_cosponsors)
         # This generator needs to yield both the bill and a separate cosponsor request
         # The two scrapes need to be separate     
         yield bill_dict
         yield cosponsors_request
     
-    # Generates all cosponsors of the bill for a separate table
+    # Generates all cosponsors of the bill for a secondary dataset. Both this and the original dictionary will
+    # flow and be filtered through the same pipeline.
     def parse_cosponsors(self, response):
         # Parses out all cosponsor information
         bill_info = response.xpath(".//h1[@class='legDetail']/text()").extract_first()  
@@ -144,9 +164,11 @@ class BillCrawlerSpider(scrapy.Spider):
         cosponsors_info = response.xpath(".//table[@class = 'item_table']/tbody/tr/td/a/text()").extract()
         bill_id = bill_info['bill_id']
         amdt_id = bill_info['amdt_id']
+
+        # Parse out all cosponsors of a bill.
         cosponsors_info = (create_pol_dict(pol) for pol in cosponsors_info)
         # There may be multiple cosponsors, so a dictionary is generated for each cosponsor
-        # Then uploaded individually
+        # then uploaded individually.
         for pol in cosponsors_info:
             cosponsor_dict = {'bill_id': bill_id,
                               'amdt_id': amdt_id,
@@ -155,5 +177,4 @@ class BillCrawlerSpider(scrapy.Spider):
                               'cosponsor_party': pol['party'],
                               'cosponsor_state': pol['state'],
                              }
-            yield cosponsor_dict;
-            
+            yield cosponsor_dict
